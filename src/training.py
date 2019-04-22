fe134000f1cca6a056e6f4b55dcc223a712791e7
kangdongwon <dongwon5520@gmail.com>
2019-04-22 22:42:36 +0900

    ver 4

--- src/training.py
+++ src/training.py
@@ -48,12 +48,12 @@ with tf.device("/gpu:0"):
             state = sess.run(init_state)
             for e in range(epochs):
                 now_time = -time.time()
-                batch_time = -time.time()
+                # batch_time = -time.time()
                 train_writer = tf.summary.FileWriter('board/train-'+str(e)+'-'+str(start_time), sess.graph)
                 for _index, (essays_, lengths_, scores_) in enumerate(parallelize_dataframe(batch_size=batch_size_), 1):
-                    get_batches_time = batch_time + time.time()
-                    get_batches_time = time.gmtime(get_batches_time)
-                    print("load data Time: {}sec...".format(get_batches_time.tm_sec))
+                    # get_batches_time = batch_time + time.time()
+                    # get_batches_time = time.gmtime(get_batches_time)
+                    # print("load data Time: {}sec...".format(get_batches_time.tm_sec))
                     essay_indice = [[index, length - 1] for index, length in enumerate(lengths_)]
                     feed = {
                         essays:     essays_,
@@ -64,17 +64,17 @@ with tf.device("/gpu:0"):
                         keep_prob:  0.5
                     }
                     loss_, state, _ = sess.run([loss_hist, final_states, optimizer], feed_dict=feed)
-                    get_batches_time = batch_time + time.time() - get_batches_time
-                    get_batches_time = time.gmtime(get_batches_time)
-                    print("sess run Time: {}sec...".format(get_batches_time.tm_sec))
-                    batch_time = -time.time()
+                    # get_batches_time = batch_time + time.time()
+                    # get_batches_time = time.gmtime(get_batches_time)
+                    # print("sess run Time: {}sec...".format(get_batches_time.tm_sec))
+                    # batch_time = -time.time()
                     if _index % 20 == 0:
                         train_writer.add_summary(loss_, _index)
 
                 now_time += time.time()
                 now_time = time.gmtime(now_time)
-                print("Epoch: {}/{}...\n".format(e + 1, epochs),
-                      "Time: {}hour {}min {}sec...".format(now_time.tm_hour, now_time.tm_min, now_time.tm_sec))
+                print("Epoch: {}/{}\t".format(e + 1, epochs),
+                      "Time: {}min {}sec".format(now_time.tm_min, now_time.tm_sec))
                 train_writer.close()
 
             # test
