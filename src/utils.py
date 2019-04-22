d8956852ba21d0a62ea4c7fc7927143a59bb079b
kangdongwon <dongwon5520@gmail.com>
2019-04-22 09:10:16 +0900

    ver 4

--- src/utils.py
+++ src/utils.py
@@ -176,13 +176,11 @@ def parallelize_dataframe(train_or_valid="train", batch_size=100):
         for index_loop in range(loop_count):
             # ret = [pool.apply_async(os.getpid, ()) for i in range(10)]
             ret = list()
+            pool.start()
             ret.extend(pool.map(load_data, filepaths[batch_size * index_batch + index_loop * num_cores:batch_size * index_batch + (index_loop+1) * num_cores]))
             pool.close()
             pool.join()
-            # print(ret)
             for sibal in ret:
-                print('t')
-                print(sibal)
                 essays_.append(sibal[0])
                 lengths_.append(sibal[1])
                 scores_.append(sibal[2])
