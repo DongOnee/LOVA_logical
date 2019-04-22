f6a824f2cd456c98f248871528c9f7f0aa0b8249
kangdongwon <dongwon5520@gmail.com>
2019-04-23 01:37:56 +0900

    ver 4

--- src/models.py
+++ src/models.py
@@ -48,8 +48,8 @@ def build_cost_fn_and_opt(lstm_outputs, indice,  scores_, learning_rate):
     predictions = tf.gather_nd(lstm_outputs, indice)  # [batchsize, cell.outputsize]
     # predictions = tf.contrib.layers.fully_connected(last_sentences, 1, activation_fn=tf.sigmoid)
 
-    # loss = tf.losses.sum_squared_error(scores_, predictions)
-    loss = tf.reduce_sum(tf.square(predictions - scores_))
+    loss = tf.losses.sum_squared_error(scores_, predictions)
+    # loss = tf.reduce_sum(tf.square(predictions - scores_))
     optimzer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
     return predictions, loss, optimzer
