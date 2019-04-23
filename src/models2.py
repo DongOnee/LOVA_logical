380eb3ca9798091eac7e709e09688b9daca8674f
kangdongwon <dongwon5520@gmail.com>
2019-04-24 01:08:05 +0900

    ver 4

--- src/models2.py
+++ src/models2.py
@@ -28,14 +28,17 @@ def build_lstm_layers(sentences, sentences_length, hidden_layer, keep_prob_):
     fw_cells = [tf.contrib.rnn.LSTMCell(layer, name='basic_lstm_cell') for layer in hidden_layer]
     fw_drops = [tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_) for lstm in fw_cells]
     fw_stacked_cell = tf.contrib.rnn.MultiRNNCell(fw_drops)
+    fw_init_state = fw_stacked_cell.zero_state(tf.shape(sentences)[0], tf.float32)
 
     bw_cells = [tf.contrib.rnn.LSTMCell(layer, name='basic_lstm_cell') for layer in hidden_layer]
     bw_drops = [tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_) for lstm in bw_cells]
     bw_stacked_cell = tf.contrib.rnn.MultiRNNCell(bw_drops)
+    bw_init_state = bw_stacked_cell.zero_state(tf.shape(sentences)[0], tf.float32)
 
-    outputs, _ = tf.nn.bidirectional_dynamic_rnn(fw_stacked_cell, bw_stacked_cell, sentences,
-                                                 sequence_length=sentences_length,
-                                                 dtype=tf.float32)
+    outputs, states = tf.nn.bidirectional_dynamic_rnn(fw_stacked_cell, bw_stacked_cell, sentences,
+                                                      sequence_length=sentences_length,
+                                                      initial_state_fw=fw_init_state,
+                                                      initial_state_bw=bw_init_state)
 
     return outputs
 
@@ -51,9 +54,9 @@ def build_cost_fn_and_opt(lstm_outputs, indice,  scores_, learning_rate, n_hidde
     outputs_fw = tf.gather_nd(lstm_outputs[0], indice)
     outputs_bw = tf.gather_nd(lstm_outputs[1], indice)
     outputs_concat = tf.concat([outputs_fw, outputs_bw], axis=1)
-    weight = tf.Variable(tf.random_normal([n_hidden * 2], seed=10))
+    weights = tf.Variable(tf.random_normal([n_hidden * 2], seed=10))
     bias = tf.Variable(tf.random_normal([1], seed=10))
-    predictions = tf.matmul(outputs_concat, weight) + bias
+    predictions = tf.matmul(outputs_concat, weights) + bias
 
     loss = tf.losses.mean_squared_error(scores_, predictions)
     # loss = tf.reduce_sum(tf.square(predictions - scores_))
