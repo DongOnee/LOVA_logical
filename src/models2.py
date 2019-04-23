2d3a974d1524b6b12eb26950cfd2f7735815fa47
kangdongwon <dongwon5520@gmail.com>
2019-04-24 01:02:58 +0900

    ver 4

--- src/models2.py
+++ src/models2.py
@@ -21,22 +21,25 @@ def model_inputs():
 def build_lstm_layers(sentences, sentences_length, hidden_layer, keep_prob_):
     """
     Create the LSTM layers
+    :parm "sentences"   : sentences [batchsize, max_len, ???]
     :parm "lstm_sizes"  : stacked lstm hidden layer size
-    :parm "embed"       : embedded sentences vector representation
     :parm "keep_prob_"  : drop out value
     """
-    embed = tf.reshape(sentences, [-1, 100, 1024])
-    cells = [tf.contrib.rnn.LSTMCell(layer, name='basic_lstm_cell') for layer in hidden_layer]
-    drops = [tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_) for lstm in cells]
-    stacked_cell = tf.contrib.rnn.MultiRNNCell(drops)
-    init_state = stacked_cell.zero_state(100, tf.float32)
+    fw_cells = [tf.contrib.rnn.LSTMCell(layer, name='basic_lstm_cell') for layer in hidden_layer]
+    fw_drops = [tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_) for lstm in fw_cells]
+    fw_stacked_cell = tf.contrib.rnn.MultiRNNCell(fw_drops)
 
-    outputs, states = tf.nn.dynamic_rnn(stacked_cell, embed, initial_state=init_state, sequence_length=sentences_length)
+    bw_cells = [tf.contrib.rnn.LSTMCell(layer, name='basic_lstm_cell') for layer in hidden_layer]
+    bw_drops = [tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_) for lstm in bw_cells]
+    bw_stacked_cell = tf.contrib.rnn.MultiRNNCell(bw_drops)
 
-    return outputs, stacked_cell, init_state, states
+    outputs, _ = tf.nn.bidirectional_dynamic_rnn(fw_stacked_cell, bw_stacked_cell, sentences,
+                                                 sequence_length=sentences_length)
+
+    return outputs
 
 
-def build_cost_fn_and_opt(lstm_outputs, indice,  scores_, learning_rate):
+def build_cost_fn_and_opt(lstm_outputs, indice,  scores_, learning_rate, n_hidden):
     """
     Create the Loss function and Optimizer
     :parm "lstm_outputs"    : output of lstm layers
@@ -44,9 +47,12 @@ def build_cost_fn_and_opt(lstm_outputs, indice,  scores_, learning_rate):
     :parm "scores_"         : true score value
     :parm "learning_rate"   : learning rate
     """
-    # last_sentences
-    predictions = tf.gather_nd(lstm_outputs, indice)  # [batchsize, cell.outputsize]
-    # predictions = tf.contrib.layers.fully_connected(last_sentences, 1, activation_fn=tf.sigmoid)
+    outputs_fw = tf.gather_nd(lstm_outputs[0], indice)
+    outputs_bw = tf.gather_nd(lstm_outputs[1], indice)
+    outputs_concat = tf.concat([outputs_fw, outputs_bw], axis=1)
+    weight = tf.Variable(tf.random_normal([n_hidden * 2], seed=10))
+    bias = tf.Variable(tf.random_normal([1], seed=10))
+    predictions = tf.matmul(outputs_concat, weight) + bias
 
     loss = tf.losses.mean_squared_error(scores_, predictions)
     # loss = tf.reduce_sum(tf.square(predictions - scores_))
--- src/training2.py
+++ src/training2.py
@@ -1,5 +1,5 @@
 import argparse
-from models import *
+from models2 import *
 from utils import *
 import tensorflow as tf
 import time
@@ -16,7 +16,7 @@ args = parser.parse_args()
 ###############
 # Hyper params
 global_step = args.step
-lstm_size = [512, 256, 1]
+lstm_size = [512, 256]
 epochs = args.epochs
 learning_rate = 0.4
 batch_size_ = 100
@@ -32,8 +32,8 @@ with tf.device("/gpu:0"):
     with tf.Graph().as_default():
         # modeling
         essays, lengths, indice, scores, keep_prob = model_inputs()
-        outputs, cell, init_state, final_states = build_lstm_layers(essays, lengths, lstm_size, keep_prob)
-        predictions, losses, optimizer = build_cost_fn_and_opt(outputs, indice, scores, learning_rate)
+        outputs = build_lstm_layers(essays, lengths, lstm_size, keep_prob)
+        predictions, losses, optimizer = build_cost_fn_and_opt(outputs, indice, scores, learning_rate, lstm_size[-1])
 
         # to Tensorboard, saver
         loss_hist = tf.summary.scalar('loss_hist', losses)
@@ -45,7 +45,6 @@ with tf.device("/gpu:0"):
             sess.run(tf.global_variables_initializer())
 
             # train
-            state = sess.run(init_state)
             for e in range(epochs):
                 now_time = -time.time()
                 # batch_time = -time.time()
@@ -60,10 +59,9 @@ with tf.device("/gpu:0"):
                         lengths:    lengths_,
                         indice:     essay_indice,
                         scores:     [[score] for score in scores_],
-                        init_state: state,
                         keep_prob:  0.5
                     }
-                    loss_, state, _ = sess.run([loss_hist, final_states, optimizer], feed_dict=feed)
+                    loss_, _ = sess.run([loss_hist, optimizer], feed_dict=feed)
                     # get_batches_time = batch_time + time.time()
                     # get_batches_time = time.gmtime(get_batches_time)
                     # print("sess run Time: {}sec...".format(get_batches_time.tm_sec))
@@ -87,7 +85,6 @@ with tf.device("/gpu:0"):
                     lengths:    lengths_,
                     indice:     essay_indice,
                     scores:     [[score] for score in scores_],
-                    init_state: state,
                     keep_prob:  1
                 }
                 loss_ = sess.run(loss_hist, feed_dict=feed)
