23dbff128d5629583a22db788a040f506d5b9006
kangdongwon <dongwon5520@gmail.com>
2019-04-24 01:56:23 +0900

    ver 4

--- src/training.py
+++ src/training.py
@@ -16,7 +16,7 @@ args = parser.parse_args()
 ###############
 # Hyper params
 global_step = args.step
-lstm_size = [512, 256, 1]
+lstm_size = [512, 256]
 epochs = args.epochs
 learning_rate = 0.4
 batch_size_ = 100
@@ -32,11 +32,12 @@ with tf.device("/gpu:0"):
     with tf.Graph().as_default():
         # modeling
         essays, lengths, indice, scores, keep_prob = model_inputs()
-        outputs, cell, init_state, final_states = build_lstm_layers(essays, lengths, lstm_size, keep_prob)
-        predictions, losses, optimizer = build_cost_fn_and_opt(outputs, indice, scores, learning_rate)
+        outputs = build_lstm_layers(essays, lengths, lstm_size, keep_prob, batch_size_)
+        predictions, losses, optimizer = build_cost_fn_and_opt(outputs, indice, scores, learning_rate, lstm_size[-1])
 
         # to Tensorboard, saver
         loss_hist = tf.summary.scalar('loss_hist', losses)
+        loss_history_index = 1
         saver = tf.train.Saver()
 
         with tf.Session() as sess:
@@ -45,7 +46,6 @@ with tf.device("/gpu:0"):
             sess.run(tf.global_variables_initializer())
 
             # train
-            state = sess.run(init_state)
             for e in range(epochs):
                 now_time = -time.time()
                 # batch_time = -time.time()
@@ -60,16 +60,16 @@ with tf.device("/gpu:0"):
                         lengths:    lengths_,
                         indice:     essay_indice,
                         scores:     [[score] for score in scores_],
-                        init_state: state,
                         keep_prob:  0.5
                     }
-                    loss_, state, _ = sess.run([loss_hist, final_states, optimizer], feed_dict=feed)
+                    loss_, _ = sess.run([loss_hist, optimizer], feed_dict=feed)
                     # get_batches_time = batch_time + time.time()
                     # get_batches_time = time.gmtime(get_batches_time)
                     # print("sess run Time: {}sec...".format(get_batches_time.tm_sec))
                     # batch_time = -time.time()
                     if _index % 20 == 0:
-                        train_writer.add_summary(loss_, _index)
+                        train_writer.add_summary(loss_, loss_history_index)
+                        loss_history_index += 1
 
                 now_time += time.time()
                 now_time = time.gmtime(now_time)
@@ -87,7 +87,6 @@ with tf.device("/gpu:0"):
                     lengths:    lengths_,
                     indice:     essay_indice,
                     scores:     [[score] for score in scores_],
-                    init_state: state,
                     keep_prob:  1
                 }
                 loss_ = sess.run(loss_hist, feed_dict=feed)
